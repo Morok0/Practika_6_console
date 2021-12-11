dca1e7326bb3fd1126dc7d00f22bf2b6744e29ba
Morok0 <95218742+Morok0@users.noreply.github.com>
2021-12-11 16:16:16 +0300

    Add files via upload

---
+++ Pr_6_zad_4(1)/Pr_6_zad_4(1)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+
+namespace Pr_6_zad_4_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int n = 0, k;
+            Console.WriteLine("\t Размер массивов:");
+            k = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("\t Введите количество массивов:");
+            n = Int32.Parse(Console.ReadLine());
+            int[][] a = new int[n][];
+            for (int i = 0; i < n; i++)
+                a[i] = new int[k];
+            Console.WriteLine("\t Исходный массив:");
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < a[i].Length; ++j)
+
+                    Console.WriteLine(a[i][j] = rnd.Next(0, 10));
+                Console.WriteLine("\r\n");
+
+            }
+
+            Console.WriteLine("\t На что меняем?");
+            int[] X = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                X[i] = rnd.Next(0, 10);
+                Console.Write(X[i] + "\n");
+
+            }
+
+
+            for (int i = 1; i < a.Length; i += 2)
+            {
+                a[i] = X;
+            }
+            Console.WriteLine("\t Изменённый массив:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    Console.WriteLine(a[i][j]);
+                }
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
